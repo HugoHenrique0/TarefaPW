eb8687c8052121853afb0891ba4bde9fe065248d
HugoHenrique0 <yalpff511@gmail.com>
2021-12-10 17:36:10 +0000

    commit 2

---
+++ proj/src/views/contents/Detalhes.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Detalhes = props => (
+    <div>
+        <h1>Detalhes</h1>
+        
+        <h2>Automóvel desportivo ou automóvel esporte, ou mais popularmente no Brasil carro esporte, é geralmente um automóvel pequeno, de dois lugares e duas portas, desenhado para resposta rápida, fácil maneabilidade, e condução de alta velocidade.
+        <br/>Carros esporte podem ser simples ou luxuosos, mas as características de grande manobrabilidade e baixo peso são requisitos essenciais.
+
+Eles podem ser equipados para corridas, "especialmente na parte aerodinâmica fazendo dele um carro de um ou dois lugares com baixo centro de gravidade e sistema de direção e suspensão projetadas para controle preciso em altas velocidades."</h2>
+
+<img src="https://www.planetcarsz.com/assets/uploads/2018/08/BUGATTI%20DIVO%202019%20002.jpg" width="720px" />
+    </div>
+        
+
+)
+
+export default Detalhes
---
+++ proj/src/views/contents/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Inicio = props => (
+    <div>
+        <h1>Projeto PW2</h1>
+        <br />
+        <h2>Carros Sports</h2> 
+        <img src="https://exame.com/wp-content/uploads/2021/07/Porsche-911-Turbo-S-1.jpg" width="720px" /> 
+    </div>
+)
+
+export default Inicio
